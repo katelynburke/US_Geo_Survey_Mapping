804d521def47c36eb5e2e227aac4959f2d0f30ce
Katelyn Burke <katelyn.burke2@gmail.com>
2019-06-17 12:28:05 -0500

    store api endpoint and get request

--- logic.js
+++ logic.js
@@ -1,1 +1,29 @@
-// Creating map 
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the queryUrl
+d3.json(queryUrl, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
+
+
+function createFeatures(earthquakeData) {
+
+    
+}
+// Create a map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+  
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets-basic",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+  // Displays map of the USA
+
