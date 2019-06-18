edff97c2d4aed5ae385267a6ded10406dc4e721d
Katelyn Burke <katelyn.burke2@gmail.com>
2019-06-18 15:10:18 -0500

    working on legend

--- js/logic.js
+++ js/logic.js
@@ -83,27 +83,45 @@ function addColor(d){
            "#cc0062";
 }
 
-function getRadius(value){
-  return value*25000
-}
+var legend = L.control({position: 'bottomleft'});
 
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-   grades = [0, 1, 2, 3, 4]
+legend.onAdd = function(myMap){
+  var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 1, 2, 3, 4, 5],
     labels = [];
 
-    // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-}
-
-return div;
+      div.innerHTML +=
+          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+  return div;
 };
 
-legend.addTo(map);
-grades = [0, 1, 2, 3, 4]
+legend.addTo(myMap);
+
+ 
+function getRadius(value){
+  return value*25000
+};
+
+// var legend = L.control({position: 'bottomright'});
+
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//    grades = [0, 1, 2, 3, 4]
+//     labels = [];
+
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+// }
+
+// return div;
+// };
+
+// legend.addTo(map);
+// grades = [0, 1, 2, 3, 4]
