09bfabb16cd88e665f6c7fc2db3aa152f4404424
Katelyn Burke <katelyn.burke2@gmail.com>
2019-06-18 15:04:09 -0500

    working on legend

--- js/logic.js
+++ js/logic.js
@@ -74,7 +74,6 @@ var myMap = L.map("map", {
 
 }
 
-
 function addColor(d){
   return d > 5 ? "#00cc69":
   d  > 4 ? "#00cc9c":
@@ -87,3 +86,24 @@ function addColor(d){
 function getRadius(value){
   return value*25000
 }
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+   grades = [0, 1, 2, 3, 4]
+    labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+}
+
+return div;
+};
+
+legend.addTo(map);
+grades = [0, 1, 2, 3, 4]
